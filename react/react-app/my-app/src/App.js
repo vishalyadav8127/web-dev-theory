c6e5261c8ad08fafa691bb54893704cbfbe82786
Vishal <vy812794@gmail.com>
2024-07-27 17:22:02 +0530

    react practice

---
+++ react/react-app/my-app/src/App.js
@@ -0,0 +1,16 @@
+import {Image,DogCard} from './DogCard'
+function App() {
+  return (
+    <div className="App">
+      <button id="btn">
+      <span>
+        Submit
+      </span>
+      </button>
+      <DogCard/>
+      <Image/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/react-app/my-app/src/DogCard.js
@@ -0,0 +1,12 @@
+export function Image(){
+    return <img src="https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+}
+export function DogCard(){
+    return(
+        <div>
+            <h3>Bruno</h3>
+            <Image/>
+        </div>
+    );
+}
+//export default DogCard;
